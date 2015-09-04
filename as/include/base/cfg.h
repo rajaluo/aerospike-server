830a6c05e3237dfb52c39bd5b5930d504d6e74f8
Brad Whisler <brad@aerospike.com>
2015-09-04 16:29:06 -0700

    Remove writes_reply histogram.

--- as/include/base/cfg.h
+++ as/include/base/cfg.h
@@ -441,7 +441,6 @@ typedef struct as_config_s {
 	cf_hist_track *		ut_hist; // histogram that tracks udf performance
 	cf_hist_track *		wt_hist; // histogram that tracks write performance
 	cf_hist_track *		px_hist; // histogram that tracks proxy performance
-	cf_hist_track *		wt_reply_hist; // write histogram from start to reply to client
 	cf_hist_track *		q_hist;  // histogram that tracks query performance
 	cf_hist_track *		q_rcnt_hist;  // histogram that tracks query row count
 
