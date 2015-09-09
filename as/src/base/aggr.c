3cea36c1aaa1e973ab31338c1af47e9bef46346f
raj <raj@aerospike.com>
2015-09-09 01:53:09 -0700

    Fix naming in aggr.c

--- as/src/base/aggr.c
+++ as/src/base/aggr.c
@@ -63,50 +63,50 @@ typedef struct {
 	// Module Data
 	as_aggr_call          * call;
 	void                  * udata;
-} aggr_record;
+} aggr_obj;
 
 static as_partition_reservation *
-ptn_reserve(aggr_record *arecord, as_partition_id pid, as_partition_reservation *rsv)
+ptn_reserve(aggr_obj *aobj, as_partition_id pid, as_partition_reservation *rsv)
 {
-	as_aggr_call *call = arecord->call;
+	as_aggr_call *call = aobj->call;
 	if (call && call->aggr_hooks && call->aggr_hooks->ptn_reserve) {
-		return call->aggr_hooks->ptn_reserve(arecord->udata, rsv->ns, pid, rsv);
+		return call->aggr_hooks->ptn_reserve(aobj->udata, rsv->ns, pid, rsv);
 	} 
 	return NULL;
 }
 
 static void
-ptn_release(aggr_record *arecord)
+ptn_release(aggr_obj *aobj)
 {
-	as_aggr_call  *call = arecord->call;
+	as_aggr_call  *call = aobj->call;
 	if (call && call->aggr_hooks && call->aggr_hooks->ptn_release) {
-		call->aggr_hooks->ptn_release(arecord->udata, arecord->rsv);	
+		call->aggr_hooks->ptn_release(aobj->udata, aobj->rsv);	
 	}
 }
 
 static void
-set_error(aggr_record *arecord, int err)
+set_error(aggr_obj *aobj, int err)
 {
-	as_aggr_call  *call = arecord->call;
+	as_aggr_call  *call = aobj->call;
 	if (call && call->aggr_hooks && call->aggr_hooks->set_error) {
-		call->aggr_hooks->set_error(arecord->udata, err);
+		call->aggr_hooks->set_error(aobj->udata, err);
 	}
 }
 
 static bool
-pre_check(aggr_record *arecord, void *skey)
+pre_check(aggr_obj *aobj, void *skey)
 {
-	as_aggr_call  *call = arecord->call;
+	as_aggr_call  *call = aobj->call;
 	if (call && call->aggr_hooks && call->aggr_hooks->pre_check) {
-		return call->aggr_hooks->pre_check(arecord->udata, as_rec_source(arecord->urec), skey);
+		return call->aggr_hooks->pre_check(aobj->udata, as_rec_source(aobj->urec), skey);
 	} 
 	return true; // if not defined pre_check succeeds
 }
 
 static int
-aopen(aggr_record *arecord, cf_digest digest) 
+aopen(aggr_obj *aobj, cf_digest digest) 
 {
-	udf_record   * urecord  = as_rec_source(arecord->urec);
+	udf_record   * urecord  = as_rec_source(aobj->urec);
 	as_index_ref   * r_ref  = urecord->r_ref;
 	as_transaction * tr     = urecord->tr;
 
@@ -114,33 +114,33 @@ aopen(aggr_record *arecord, cf_digest digest)
 	urecord->keyd = digest; 
 
 	AS_PARTITION_RESERVATION_INIT(tr->rsv);	
-	arecord->rsv        = ptn_reserve(arecord, pid, &tr->rsv);
-	if (!arecord->rsv) {
+	aobj->rsv        = ptn_reserve(aobj, pid, &tr->rsv);
+	if (!aobj->rsv) {
 		cf_debug(AS_AGGR, "Reservation not done for partition %d", pid);
 		return -1; 
 	}
 	
 	// NB: Partial Initialization due to heaviness. Not everything needed
 	// TODO: Make such initialization Commodity
-	tr->rsv.state       = arecord->rsv->state;
-	tr->rsv.pid         = arecord->rsv->pid;
-	tr->rsv.p           = arecord->rsv->p;
-	tr->rsv.tree        = arecord->rsv->tree;
-	tr->rsv.cluster_key = arecord->rsv->cluster_key;
-	tr->rsv.sub_tree    = arecord->rsv->sub_tree;
+	tr->rsv.state       = aobj->rsv->state;
+	tr->rsv.pid         = aobj->rsv->pid;
+	tr->rsv.p           = aobj->rsv->p;
+	tr->rsv.tree        = aobj->rsv->tree;
+	tr->rsv.cluster_key = aobj->rsv->cluster_key;
+	tr->rsv.sub_tree    = aobj->rsv->sub_tree;
 	tr->keyd            = urecord->keyd;
 
 	r_ref->skip_lock    = false;
 	if (udf_record_open(urecord) == 0) { 
-		arecord->open   = true;
+		aobj->open   = true;
 		return 0;
 	}
-	ptn_release(arecord);
+	ptn_release(aobj);
 	return -1;
 }
 
 void
-aclose(aggr_record *arecord)
+aclose(aggr_obj *aobj)
 {
 	// Bypassing doing the direct destroy because we need to
 	// avoid reducing the ref count. This rec (query_record
@@ -148,22 +148,22 @@ aclose(aggr_record *arecord)
 	// here to Lua. If Lua access it even after moving to next
 	// element in the stream it does it at its own risk. Record
 	// may have changed under the hood.
-	if (arecord->open) {
-		udf_record_close(as_rec_source(arecord->urec));
-		ptn_release(arecord);
-		arecord->open = false;
+	if (aobj->open) {
+		udf_record_close(as_rec_source(aobj->urec));
+		ptn_release(aobj);
+		aobj->open = false;
 	}
 	return;
 }
 
 void
-acleanup(aggr_record *arecord) 
+acleanup(aggr_obj *aobj) 
 {
-	if (arecord->iter) {
-		cf_ll_releaseIterator(arecord->iter);
-		arecord->iter = NULL;
+	if (aobj->iter) {
+		cf_ll_releaseIterator(aobj->iter);
+		aobj->iter = NULL;
 	}
-	aclose(arecord);
+	aclose(aobj);
 }
 
 // **************************************************************************************************
@@ -173,41 +173,41 @@ acleanup(aggr_record *arecord)
  */
 // **************************************************************************************************
 cf_digest *
-get_next(aggr_record *arecord)
+get_next(aggr_obj *aobj)
 {
-	if (!arecord->keys_arr) {
-		cf_ll_element * ele       = cf_ll_getNext(arecord->iter);
+	if (!aobj->keys_arr) {
+		cf_ll_element * ele       = cf_ll_getNext(aobj->iter);
 		if (!ele) {
-			arecord->keys_arr = NULL;
+			aobj->keys_arr = NULL;
 			cf_detail(AS_AGGR, "No more digests found in agg stream");	
 		}
 		else {
-			arecord->keys_arr = ((as_index_keys_ll_element*)ele)->keys_arr;
+			aobj->keys_arr = ((as_index_keys_ll_element*)ele)->keys_arr;
 		}
-		arecord->keys_arr_offset  = 0;
+		aobj->keys_arr_offset  = 0;
 	} 
-	as_index_keys_arr  * keys_arr  = arecord->keys_arr;
+	as_index_keys_arr  * keys_arr  = aobj->keys_arr;
 
 	if (!keys_arr) {
 		cf_debug(AS_AGGR, "No digests found in agg stream");
 		return NULL;
 	}
 
-	if (keys_arr->num == arecord->keys_arr_offset) {
-		cf_ll_element * ele  = cf_ll_getNext(arecord->iter);
+	if (keys_arr->num == aobj->keys_arr_offset) {
+		cf_ll_element * ele  = cf_ll_getNext(aobj->iter);
 		if (!ele) {
 			cf_detail(AS_AGGR, "No More Nodes for this Lua Call");
 			return NULL;
 		}
-		keys_arr                 = ((as_index_keys_ll_element*)ele)->keys_arr;
-		arecord->keys_arr_offset = 0;
-		arecord->keys_arr        = keys_arr;
+		keys_arr              = ((as_index_keys_ll_element*)ele)->keys_arr;
+		aobj->keys_arr_offset = 0;
+		aobj->keys_arr        = keys_arr;
 		cf_detail(AS_AGGR, "Moving to next node of digest list");
 	} else {
-		arecord->keys_arr_offset++;
+		aobj->keys_arr_offset++;
 	}
 
-	return &arecord->keys_arr->pindex_digs[arecord->keys_arr_offset];
+	return &aobj->keys_arr->pindex_digs[aobj->keys_arr_offset];
 }
 
 // only operates on the record as_val in the stream points to
@@ -218,26 +218,26 @@ get_next(aggr_record *arecord)
 static as_val *
 istream_read(const as_stream *s) 
 {
-	aggr_record *arecord = as_stream_source(s);
+	aggr_obj *aobj = as_stream_source(s);
 
-	aclose(arecord);
+	aclose(aobj);
 
 	// Iterate through stream to get next digest and
 	// populate record with it
-	while (!arecord->open) {
+	while (!aobj->open) {
 		
-		if (get_next(arecord)) { 
+		if (get_next(aobj)) { 
 			return NULL;
 		}
 
-		if (!aopen(arecord, arecord->keys_arr->pindex_digs[arecord->keys_arr_offset])) {
-			if (!pre_check(arecord, &arecord->keys_arr->sindex_keys[arecord->keys_arr_offset])) {
-				aclose(arecord);
+		if (!aopen(aobj, aobj->keys_arr->pindex_digs[aobj->keys_arr_offset])) {
+			if (!pre_check(aobj, &aobj->keys_arr->sindex_keys[aobj->keys_arr_offset])) {
+				aclose(aobj);
 			}
 		}
 	}
-	as_val_reserve(arecord->urec);
-	return (as_val *)arecord->urec;
+	as_val_reserve(aobj->urec);
+	return (as_val *)aobj->urec;
 }
 
 const as_stream_hooks istream_hooks = {
@@ -256,8 +256,8 @@ const as_stream_hooks istream_hooks = {
 as_stream_status
 ostream_write(const as_stream *s, as_val *val)
 {
-	aggr_record *arecord = (aggr_record *)as_stream_source(s);
-	return arecord->call->aggr_hooks->ostream_write(arecord->udata, val);
+	aggr_obj *aobj = (aggr_obj *)as_stream_source(s);
+	return aobj->call->aggr_hooks->ostream_write(aobj->udata, val);
 }
 
 const as_stream_hooks ostream_hooks = {
@@ -313,7 +313,7 @@ as_aggr_process(as_namespace *ns, as_aggr_call * ag_call, cf_ll * ap_recl, void 
 	urecord.rd      = &rd;
 	as_rec   * urec = as_rec_new(&urecord, &udf_record_hooks);
 
-	aggr_record arecord = {
+	aggr_obj aobj = {
 		.iter            = cf_ll_getIterator(ap_recl, true /*forward*/),
 		.urec            = urec,
 		.keys_arr        = NULL,
@@ -324,7 +324,7 @@ as_aggr_process(as_namespace *ns, as_aggr_call * ag_call, cf_ll * ap_recl, void 
 		.rsv             = &tr.rsv
 	};
 
-	if (!arecord.iter) {
+	if (!aobj.iter) {
 		cf_warning (AS_AGGR, "Could not set up iterator .. possibly out of memory .. Aborting Query !!");
 		return AS_AGGR_ERR;
 	}
@@ -334,11 +334,11 @@ as_aggr_process(as_namespace *ns, as_aggr_call * ag_call, cf_ll * ap_recl, void 
 
 	// Input Stream
 	as_stream istream;
-	as_stream_init(&istream, &arecord, &istream_hooks);
+	as_stream_init(&istream, &aobj, &istream_hooks);
 
 	// Output stream
 	as_stream ostream;
-	as_stream_init(&ostream, &arecord, &ostream_hooks);
+	as_stream_init(&ostream, &aobj, &ostream_hooks);
 
 	// Argument list
 	as_list arglist;
@@ -355,6 +355,6 @@ as_aggr_process(as_namespace *ns, as_aggr_call * ag_call, cf_ll * ap_recl, void 
 
 	as_list_destroy(&arglist);
 
-	acleanup(&arecord);
+	acleanup(&aobj);
 	return ret;
 }
