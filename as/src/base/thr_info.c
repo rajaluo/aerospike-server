9c9f1c3c1a70538ddeec9fc3b2d4f968c4bd6ad7
Thomas Lopatic <thomas@aerospike.com>
2015-09-14 18:30:23 +0200

    Properly signal write error.

--- as/src/base/thr_info.c
+++ as/src/base/thr_info.c
@@ -4499,6 +4499,8 @@ thr_info_fn(void *unused)
 				} else {
 					cf_info(AS_INFO, "thr_info: can't write all bytes, fd %d error %d", tr->proto_fd_h->fd, errno);
 				}
+				as_end_of_transaction_force_close(tr->proto_fd_h);
+				tr->proto_fd_h = 0;
 				break;
 			}
 			else if (rv > 0)
@@ -4511,8 +4513,10 @@ thr_info_fn(void *unused)
 
 		cf_free(tr->msgp);
 
-		as_end_of_transaction(tr->proto_fd_h);
-		tr->proto_fd_h = 0;
+		if (tr->proto_fd_h) {
+			as_end_of_transaction(tr->proto_fd_h);
+			tr->proto_fd_h = 0;
+		}
 
 		MICROBENCHMARK_HIST_INSERT_P(info_fulfill_hist);
 	}
