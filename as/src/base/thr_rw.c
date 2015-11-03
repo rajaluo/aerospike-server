3f44e04d9f319619bc650fd82a07403ec6ec2c02
Andrew Gooding <andy@aerospike.com>
2015-11-03 11:15:43 -0800

    Removed trailing whitespace, fixed comments, minor code cleanup, tweaked warning text.

--- as/src/base/thr_rw.c
+++ as/src/base/thr_rw.c
@@ -3041,12 +3041,12 @@ write_delete_local(as_transaction *tr, bool journal, cf_node masternode,
 				SINDEX_BINS_SETUP(sbins, ns->sindex_cnt);
 				as_sindex * si_arr[ns->sindex_cnt];
 				int si_arr_index = 0;
-				int si_cnt = 0;	
+				int si_cnt = 0;
 				int sbins_populated = 0;
 
 				// RESERVE MATCHING SIs
 				for (int i=0; i<rd.n_bins; i++) {
-					si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name, rd.bins[i].id, 
+					si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name, rd.bins[i].id,
 								&si_arr[si_arr_index]);
 					si_arr_index += si_cnt;
 				}
@@ -3605,28 +3605,29 @@ write_local_sindex_update(as_namespace *ns, const char *set_name,
 		not_just_created[i_new] = false;
 	}
 
+	// Maximum number of sindexes which can be changed in one transaction is
+	// 2 * ns->sindex_cnt.
+
 	SINDEX_BINS_SETUP(sbins, 2 * ns->sindex_cnt);
-	as_sindex * si_arr[2 * ns->sindex_cnt];
+	as_sindex *si_arr[2 * ns->sindex_cnt];
 	int si_arr_index = 0;
 	int si_cnt = 0;
 
 	SINDEX_GRLOCK();
 
-	// RESERVE macthing SIs
-	for (int i=0; i<n_old_bins; i++) {
-		si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name, old_bins[i].id, 
-							&si_arr[si_arr_index]);
-		si_arr_index += si_cnt;
-	}
-	
-	for (int i=0; i<n_new_bins; i++) {
-		si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name, new_bins[i].id, 
-							&si_arr[si_arr_index]);
+	// Reserve matching SIs.
+
+	for (int i = 0; i < n_old_bins; i++) {
+		si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name,
+				old_bins[i].id, &si_arr[si_arr_index]);
 		si_arr_index += si_cnt;
 	}
 
-	// Maximum number of sindexes which can be changed in one transaction is
-	// 2 * ns->sindex_cnt.
+	for (int i = 0; i < n_new_bins; i++) {
+		si_cnt = as_sindex_arr_lookup_by_set_binid_lockfree(ns, set_name,
+				new_bins[i].id, &si_arr[si_arr_index]);
+		si_arr_index += si_cnt;
+	}
 
 	// For every old bin, find the corresponding new bin (if any) and adjust the
 	// secondary index if the bin was modified. If no corresponding new bin is
@@ -3690,8 +3691,7 @@ write_local_sindex_update(as_namespace *ns, const char *set_name,
 
 	SINDEX_GUNLOCK();
 
-	bool ret_val = false;
-	if (sbins_populated) {
+	if (sbins_populated != 0) {
 		uint64_t start_ns = g_config.microbenchmarks ? cf_getns() : 0;
 
 		as_sindex_update_by_sbin(ns, set_name, sbins, sbins_populated, keyd);
@@ -3700,10 +3700,11 @@ write_local_sindex_update(as_namespace *ns, const char *set_name,
 		if (start_ns != 0) {
 			histogram_insert_data_point(g_config.write_sindex_hist, start_ns);
 		}
-		ret_val =  true;
 	}
+
 	as_sindex_release_arr(si_arr, si_arr_index);
-	return ret_val;
+
+	return sbins_populated != 0;
 }
 
 void
