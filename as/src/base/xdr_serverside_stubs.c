2319652162a7464374b89548f9cc6fd03c31c93d
Wei-Ling Chu <weiling@aerospike.com>
2016-02-09 17:57:19 -0800

    update license

--- as/src/base/xdr_serverside_stubs.c
+++ as/src/base/xdr_serverside_stubs.c
@@ -1,7 +1,7 @@
 /*
  * xdr_serverside_stubs.c
  *
- * Copyright (C) 2014 Aerospike, Inc.
+ * Copyright (C) 2014-2016 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
