1539739ff83d33aedef06cbd034f8bca54c4a38d
Andrew Gooding <andy@aerospike.com>
2017-05-05 11:38:04 -0700

    AER-5628 - fixed rack-aware loop to avoid possible memory overwrite.

--- as/src/fabric/partition_balance.c
+++ as/src/fabric/partition_balance.c
@@ -937,7 +937,7 @@ partition_cluster_topology_info()
 	cc_cluster_config_defaults(&cc);
 
 	for (uint32_t cur_n = 0;
-			succession[cur_n] != (cf_node)0 && cur_n < g_paxos->cluster_size;
+			cur_n < g_paxos->cluster_size && succession[cur_n] != (cf_node)0;
 			cur_n++) {
 		cc_group_t cur_group = cc_compute_group_id(succession[cur_n]);
 
